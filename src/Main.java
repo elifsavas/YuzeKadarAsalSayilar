1504e0af601f450a713ae158ba6dc20b7ad6c9d6
Elif Savaş <98321352+elifsavas@users.noreply.github.com>
2022-11-18 15:19:30 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int i, j;
+        int sayac = 0 ;
+
+        for (i = 2; i <= 100; i++) {
+            for (j = 2; j <= i; j++) {
+                if (i % j == 0) {
+                    sayac++;
+                }
+            }
+            if (sayac == 1) {
+                System.out.println(i);
+            }
+            sayac = 0;
+
+
+        }
+
+
     }
 }
+
+
+
